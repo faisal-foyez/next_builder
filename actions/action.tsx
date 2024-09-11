3866ce2ab9e5e18a6ef2c0e0a7f022dfd428fb5c
faisal-foyez <faisalislam64@gmail.com>
2024-09-11 15:44:28 +0600

    implement server action

--- actions/action.tsx
+++ actions/action.tsx
@@ -1,5 +1,7 @@
 'use server';
 
-export async function createAction(name:string){
+export async function createAction(prevState:any, formData:FormData){
+  const name = formData.get("name");
   console.log(name);
+  return name
 }
--- app/components/UserForm.tsx
+++ app/components/UserForm.tsx
@@ -1,13 +1,13 @@
-
 import {createAction} from '@/actions/action';
-export default function SubmissionForm () {
-  const handleClick = () =>{
-    createAction('jamal');
-  }
-  return (
-    <div>
-      <button onClick={handleClick}>user form</button>
-    </div>
 
+export default function SubmissionForm () {
+  return (
+    <form action={createAction}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" name="name" className='w-28 h-5 text-black'/>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   )
 }
