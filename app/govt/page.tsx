39022e3b6bab15fa01c01c0c48ce50423c9fd4dd
faisal-foyez <faisalislam64@gmail.com>
2024-09-13 03:59:21 +0600

    Complete the Image of next.js

--- app/govt/page.tsx
+++ app/govt/page.tsx
@@ -5,10 +5,16 @@ export default function Govt () {
   
   return (
     <div>
-      <Image 
+      {/* <Image 
         src={govtImg}
         alt="Interim Govt. Advisors Image"
-      />
+      /> */}
+      <Image
+      src="https://s3.amazonaws.com/my-bucket/profile.png"
+      alt="Picture of the author"
+      width={500}
+      height={500}
+    />
     </div>
   )
 }
